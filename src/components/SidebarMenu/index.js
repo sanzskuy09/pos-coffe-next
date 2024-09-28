26ec0d036814072f3ece63488a0d419568a91a05
Ihsan <mn.ihsan09@gmail.com>
2024-09-28 21:53:41 +0700

    feat: update style jika link aktif

--- src/components/SidebarMenu/index.js
+++ src/components/SidebarMenu/index.js
@@ -1,12 +1,18 @@
+"use client";
 import React from "react";
-
+import { useRouter, usePathname } from "next/navigation";
 import Link from "next/link";
 
 const SidebarMenu = () => {
+  const pathname = usePathname();
   const date = new Date().toDateString();
+  // const isActive = false;
 
   const styleLink =
     "h-10 flex items-center px-8 font-medium text-lg hover:text-primary hover:border-r-4 hover:border-primary duration-200 transition";
+
+  const isActive = (path) =>
+    pathname === path ? "text-primary border-r-4 border-primary" : "";
 
   return (
     <div className="max-h-screen fixed bottom-0 top-0 left-0 w-72 border-r border-r-gray-200 ">
@@ -18,16 +24,25 @@ const SidebarMenu = () => {
       </div>
 
       <div className="flex flex-col gap-4">
-        <Link href={"/order/dashboard"} className={styleLink}>
+        <Link
+          href={"/order/dashboard"}
+          className={`${styleLink} ${isActive("/order/dashboard")}`}
+        >
           Dashboard
         </Link>
-        <Link href={"/order/menu"} className={styleLink}>
+        <Link
+          href={"/order/menu"}
+          className={`${styleLink} ${isActive("/order/menu")}`}
+        >
           Menu
         </Link>
-        <Link href={"/order/my-order"} className={styleLink}>
+        {/* <Link href={"/order/my-order"} className={`${styleLink} ${isActive("/order/my-order")}`}>
           My Orders
-        </Link>
-        <Link href={"/order/history"} className={styleLink}>
+        </Link> */}
+        <Link
+          href={"/order/history"}
+          className={`${styleLink} ${isActive("/order/history")}`}
+        >
           History
         </Link>
       </div>
